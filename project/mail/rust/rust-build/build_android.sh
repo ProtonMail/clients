d4ce109e5a8dc10dfe5c2cf9dd3be3b4732a422e
Leander Beernaert <leander.beernaert@proton.ch>
2026-03-10 15:34:56 +0100

    chore(mail): Remove search feature from default mobile flags

--- project/mail/rust/rust-build/build_android.sh
+++ project/mail/rust/rust-build/build_android.sh
@@ -7,7 +7,6 @@ set -eu
 export LIBSQLITE3_FLAGS="-DNDEBUG=1 -DSQLITE_POWERSAFE_OVERWRITE=1 -DSQLITE_THREADSAFE=2\
  -DSQLITE_DEFAULT_FILE_PERMISSIONS=0600 -DSQLITE_SECURE_DELETE -DSQLITE_ENABLE_BATCH_ATOMIC_WRITE"
 
-
 TARGET=$1
 CONFIG_PATH=$2
 OUT_DIR=$3
@@ -15,37 +14,37 @@ PROFILE="android"
 LIB_NAME="lib$(echo $1 | tr '-' '_').so"
 
 mkdir -p $OUT_DIR/java \
-$OUT_DIR/jniLibs/arm64-v8a \
-$OUT_DIR/jniLibs/armeabi-v7a \
-$OUT_DIR/jniLibs/x86_64 \
+  $OUT_DIR/jniLibs/arm64-v8a \
+  $OUT_DIR/jniLibs/armeabi-v7a \
+  $OUT_DIR/jniLibs/x86_64
 
 # Optional features to enable
 # - foundation_search: local body text search indexing
-FEATURES="${CARGO_FEATURES:-foundation_search}"
+FEATURES="${CARGO_FEATURES:-}"
 
+echo "Features = $FEATURES"
 # Build project
 cargo ndk -t "armeabi-v7a" -t "arm64-v8a" -t "x86_64" build --profile $PROFILE -p $TARGET --features "$FEATURES"
 
 # Generate bindings
 cargo run \
-    --release \
-    -p mail-uniffi-bindgen generate \
-    --library target/aarch64-linux-android/$PROFILE/${LIB_NAME} \
-    --language kotlin \
-    --config ${CONFIG_PATH} \
-    --out-dir $OUT_DIR/java \
-    --no-format
+  --release \
+  -p mail-uniffi-bindgen generate \
+  --library target/aarch64-linux-android/$PROFILE/${LIB_NAME} \
+  --language kotlin \
+  --config ${CONFIG_PATH} \
+  --out-dir $OUT_DIR/java \
+  --no-format
 
 # Strip symbols
 OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
 cp target/aarch64-linux-android/$PROFILE/$LIB_NAME $OUT_DIR/jniLibs/arm64-v8a/$LIB_NAME
 cp target/armv7-linux-androideabi/$PROFILE/$LIB_NAME $OUT_DIR/jniLibs/armeabi-v7a/$LIB_NAME
-cp target/x86_64-linux-android/$PROFILE/$LIB_NAME  $OUT_DIR/jniLibs/x86_64/$LIB_NAME
-
+cp target/x86_64-linux-android/$PROFILE/$LIB_NAME $OUT_DIR/jniLibs/x86_64/$LIB_NAME
 
 PGP_SYS_LIB="libgopenpgp-sys.so"
 if [[ -f "target/aarch64-linux-android/$PROFILE/$PGP_SYS_LIB" ]]; then
-    cp target/aarch64-linux-android/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/arm64-v8a/$PGP_SYS_LIB
-    cp target/armv7-linux-androideabi/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/armeabi-v7a/$PGP_SYS_LIB
-    cp target/x86_64-linux-android/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/x86_64/$PGP_SYS_LIB
+  cp target/aarch64-linux-android/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/arm64-v8a/$PGP_SYS_LIB
+  cp target/armv7-linux-androideabi/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/armeabi-v7a/$PGP_SYS_LIB
+  cp target/x86_64-linux-android/$PROFILE/$PGP_SYS_LIB $OUT_DIR/jniLibs/x86_64/$PGP_SYS_LIB
 fi

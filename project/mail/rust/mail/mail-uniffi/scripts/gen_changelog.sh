e1676a7d9f2849e0b28b90543f2ca262eb13ab55
Wojciech Polak <wojciech.polak@external.proton.ch>
2026-04-17 17:15:02 +0000

    chore: Filter out non-mail tags from changelog - proton/clients/monorepo!1687

--- project/mail/rust/mail/mail-uniffi/scripts/gen_changelog.sh
+++ project/mail/rust/mail/mail-uniffi/scripts/gen_changelog.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 INIT=mail-uniffi-v0.163.4
 
-uv run --project scripts/changelog changelog "$@" --init $INIT --path project/mail/rust --repo ../../../ >mail/mail-uniffi/CHANGELOG.md
+uv run --project scripts/changelog changelog "$@" --init $INIT --path project/mail/rust --repo ../../../ --only "^mail-uniffi-" >mail/mail-uniffi/CHANGELOG.md
 
 cat mail/mail-uniffi/CHANGELOG.old.md >>mail/mail-uniffi/CHANGELOG.md

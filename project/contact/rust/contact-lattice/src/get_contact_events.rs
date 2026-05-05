bc3e5f38525963eecb13b86ae69706127d85855f
Leander Beernaert <leander.beernaert@proton.ch>
2026-05-05 11:20:04 +0200

    feat(IMP-16): Lattice based contacts API
    Prepare network requests for contacts using lattice.
    
    We also introduce a new `ContactGroupId` type. While they technically
    use the same routes as labels, it will allow for more isolation in
    the code.
    
    For compatibility in the transition period a conversion to `LabelId` is
    available under the `mail-utils` feature. This will be removed in the
    future.

---
+++ project/contact/rust/contact-lattice/src/get_contact_events.rs
@@ -0,0 +1,47 @@
+use std::borrow::Cow;
+
+use crate::CONTACTS_V6;
+use lattice::{LtContract, LtNoQueryParams, LtRawBody, LtSlimAPIJSON};
+use mail_api_event_types::{EventId, GetEventsLatestResponse};
+
+pub struct GetLatestContactEvent;
+
+impl LtContract for GetLatestContactEvent {
+    type Response = LtSlimAPIJSON<GetEventsLatestResponse>;
+    type Body<'b> = LtSlimAPIJSON<()>;
+    type Query<'q> = LtNoQueryParams;
+
+    fn path<'a>(&'a self) -> Result<std::borrow::Cow<'a, str>, lattice::LatticeError> {
+        Ok(Cow::Owned(format!("{CONTACTS_V6}/events")))
+    }
+}
+
+#[cfg(feature = "mocks")]
+impl GetLatestContactEvent {
+    pub fn mock() -> wiremock::MockBuilder {
+        use wiremock::matchers::*;
+        wiremock::Mock::given(method("GET")).and(path(format!("api{CONTACTS_V6}/events")))
+    }
+}
+
+pub struct GetContactEvent {
+    pub id: EventId,
+}
+
+impl LtContract for GetContactEvent {
+    type Response = LtRawBody;
+    type Body<'b> = LtSlimAPIJSON<()>;
+    type Query<'q> = LtNoQueryParams;
+
+    fn path<'a>(&'a self) -> Result<Cow<'a, str>, lattice::LatticeError> {
+        Ok(Cow::Owned(format!("{CONTACTS_V6}/events/{}", self.id)))
+    }
+}
+
+#[cfg(feature = "mocks")]
+impl GetContactEvent {
+    pub fn mock(id: EventId) -> wiremock::MockBuilder {
+        use wiremock::matchers::*;
+        wiremock::Mock::given(method("GET")).and(path(format!("api{CONTACTS_V6}/events/{id}")))
+    }
+}
---
+++ project/contact/rust/contact-lattice/src/lib.rs
@@ -0,0 +1,186 @@
+mod get_contact;
+mod get_contact_events;
+mod get_contacts_emails;
+mod put_delete_contacts;
+
+pub use get_contact::*;
+pub use get_contact_events::*;
+pub use get_contacts_emails::*;
+use proton_crypto_account::contacts::ContactCardType;
+pub use put_delete_contacts::*;
+
+use mail_api_event_types::Action;
+use mail_proton_ids::PrivateEmail;
+use serde::Deserialize;
+use serde_repr::{Deserialize_repr, Serialize_repr};
+use serde_with::{BoolFromInt, serde_as};
+
+const CONTACTS_V4: &str = "/contacts/v4/contacts";
+const CONTACTS_V6: &str = "/contacts/v6";
+
+mail_proton_ids::declare_proton_id! {
+    pub ContactId
+}
+
+mail_proton_ids::declare_proton_id! {
+    pub ContactEmailId
+}
+
+mail_proton_ids::declare_proton_id! {
+    pub ContactUID
+}
+
+mail_proton_ids::declare_proton_id! {
+    pub ContactGroupId
+}
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Deserialize_repr,
+    Eq,
+    Hash,
+    PartialEq,
+    Serialize_repr
+)]
+#[repr(u8)]
+pub enum ContactSendingPreferences {
+    Custom = 0,
+    Default = 1,
+}
+
+/// Represents partial contact information returned by the API.
+///
+/// The partial contact information does not contain the contact emails and the
+/// v-cards.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactBasic {
+    #[serde(rename = "ID")]
+    pub id: ContactId,
+    pub create_time: u64,
+    #[serde(rename = "LabelIDs")]
+    pub label_ids: Vec<ContactGroupId>,
+    pub modify_time: u64,
+    pub name: String,
+    pub size: u64,
+    #[serde(rename = "UID")]
+    pub uid: ContactUID,
+}
+
+/// Represents a contact card returned by the API.
+///
+/// Contact cards contain information encoded as a v-card. Cards can be
+/// encrypted or signed with the user keys.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactCard {
+    #[serde(rename = "Type")]
+    pub card_type: ContactCardType,
+    pub data: String,
+    pub signature: Option<String>,
+}
+
+/// Models the contact email addresses for a contact returned by the API.
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactEmail {
+    #[serde(rename = "ID")]
+    pub id: ContactEmailId,
+    #[serde(rename = "ContactID")]
+    pub contact_id: ContactId,
+    pub canonical_email: PrivateEmail,
+    #[serde(rename = "Type")]
+    pub contact_type: Vec<String>,
+    pub defaults: ContactSendingPreferences,
+    pub email: PrivateEmail,
+    #[serde_as(as = "BoolFromInt")]
+    pub is_proton: bool,
+    #[serde(rename = "LabelIDs")]
+    pub label_ids: Vec<ContactGroupId>,
+    pub last_used_time: u64,
+    pub name: String,
+    pub order: u32,
+}
+
+/// Data for an event related to a [`ContactEmail`] record.
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactEmailEvent {
+    #[serde(rename = "ID")]
+    pub id: ContactEmailId,
+    pub action: Action,
+    pub contact_email: Option<ContactEmail>,
+}
+
+/// Data for an event related to a [`ContactBasic`] record.
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactEvent {
+    #[serde(rename = "ID")]
+    pub id: ContactId,
+    pub action: Action,
+    pub contact: Option<ContactFull>,
+}
+
+/// A complete contact returned by the API.
+///
+/// Compared to the [`ContactBasic`], it additionally includes all associated
+/// contact emails ([`ContactEmail`]) and cards ([`ContactCard`]).
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactFull {
+    #[serde(rename = "ID")]
+    pub id: ContactId,
+    pub cards: Vec<ContactCard>,
+    pub contact_emails: Vec<ContactEmail>,
+    pub create_time: u64,
+    #[serde(rename = "LabelIDs")]
+    pub label_ids: Vec<ContactGroupId>,
+    pub modify_time: u64,
+    pub name: String,
+    pub size: u64,
+    #[serde(rename = "UID")]
+    pub uid: ContactUID,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactRootEventV6 {
+    pub contacts: Option<Vec<ContactEventV6>>,
+    pub labels: Option<Vec<ContactLabelEventV6>>,
+    pub refresh: bool,
+    /// Whether we need to request more events after this.
+    #[serde(rename = "More")]
+    pub has_more: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactEventV6 {
+    #[serde(rename = "ID")]
+    pub id: ContactId,
+    pub action: Action,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
+#[cfg_attr(feature = "mocks", derive(serde::Serialize))]
+#[serde(rename_all = "PascalCase")]
+pub struct ContactLabelEventV6 {
+    #[serde(rename = "ID")]
+    pub id: ContactGroupId,
+    pub action: Action,
+}

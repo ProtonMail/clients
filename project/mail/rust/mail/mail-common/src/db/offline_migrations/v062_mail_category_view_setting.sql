4575e9fb92c44ce2d9f84dc83106386fb1d27710
Pawel Rynowiecki <pawel.rynowiecki@proton.ch>
2026-05-08 16:50:36 +0200

    chore: dedup v062 mail db migration after rebase

--- project/mail/rust/mail/mail-common/src/db/offline_migrations/v062_mail_category_view_setting.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE mail_settings ADD COLUMN mail_category_view INTEGER NOT NULL DEFAULT 0;
-
--- Force re-fetch of mail_settings so the real mail_category_view value is pulled from the API.
--- mail_user_context must also be reset because initialize_context() has an early-exit guard
--- at MailUserContext::is_initialized() that skips all sub-component inits when already Succeeded.
-DELETE FROM initialized_components WHERE key = 'mail_settings' OR key = 'mail_user_context';

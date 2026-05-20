5a2a027f015d31bf01c1ce17a734a1adee509894
Pawel Rynowiecki <pawel.rynowiecki@proton.ch>
2026-05-20 16:05:59 +0200

    refactor*: Preparing scroller for interruption feature

--- project/mail/rust/mail/mail-common/src/db/offline_migrations/v065_clear_labels_count.sql
+++ project/mail/rust/mail/mail-common/src/db/offline_migrations/v065_clear_labels_count.sql
@@ -1,3 +1,7 @@
 -- Live devices have incorrect counts for the Category Labels since we only care about
 -- Category Labels counts in Inbox and request is being modified in this commit
-DELETE FROM initialized_components WHERE key = 'label_counters' OR key = 'mail_user_context';
+DELETE FROM
+  initialized_components
+WHERE
+  key = 'label_counters'
+  OR key = 'mail_user_context';

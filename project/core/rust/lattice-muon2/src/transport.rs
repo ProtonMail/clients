4a1080ebc24adaab60dfd601ed19a9f8fbac3113
Laurent Gaucheron <laurent.gaucheron@proton.ch>
2026-05-22 10:18:31 +0000

    chore(core): Upgrade muon2 version. - proton/clients/monorepo!2577

--- project/core/rust/lattice-muon2/src/transport.rs
+++ project/core/rust/lattice-muon2/src/transport.rs
@@ -1,15 +1,18 @@
 use lattice::{LtTransportProvider, LtWireRequestProvider};
+#[allow(deprecated)] // Requires SDK support to migrate. See UFC-158
 use muon::SendRequest;
 use muon::http::{HttpReq, HttpRes};
 
 use crate::{LtTransportError, Muon2WireRequestProvider};
 
+#[allow(deprecated)] // Requires SDK support to migrate. See UFC-158
 pub struct Muon2Transport<
     Sender: SendRequest<HttpReq, HttpRes, Err: Into<muon::Error> + Send> + Send + Sync,
 > {
     sender: Sender,
 }
 
+#[allow(deprecated)] // Requires SDK support to migrate. See UFC-158
 impl<Sender: SendRequest<HttpReq, HttpRes, Err: Into<muon::Error> + Send> + Send + Sync>
     Muon2Transport<Sender>
 {
@@ -18,6 +21,7 @@ impl<Sender: SendRequest<HttpReq, HttpRes, Err: Into<muon::Error> + Send> + Send
     }
 }
 
+#[allow(deprecated)] // Requires SDK support to migrate. See UFC-158
 impl<Sender: SendRequest<HttpReq, HttpRes, Err: Into<muon::Error> + Send> + Send + Sync>
     LtTransportProvider for Muon2Transport<Sender>
 {
@@ -29,6 +33,7 @@ impl<Sender: SendRequest<HttpReq, HttpRes, Err: Into<muon::Error> + Send> + Send
         request: &<Self::WireProvider as LtWireRequestProvider>::Request,
     ) -> Result<<Self::WireProvider as LtWireRequestProvider>::Response, Self::Error> {
         let request = request.clone();
+        #[allow(deprecated)] // Requires SDK support to migrate. See UFC-158
         let response = self
             .sender
             .send(request)
